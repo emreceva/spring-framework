321eb04548d57a783c72cd046e213c68e084bae8
emreceva <86777959+emreceva@users.noreply.github.com>
2022-09-04 12:27:51 +0300

    Bean Scope

---
+++ Spring-05-Core-BeanScope/src/main/java/com/ceva/proxy/CommentPushNotificationProxy.java
@@ -0,0 +1,15 @@
+package com.ceva.proxy;
+
+import com.ceva.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("PUSH")
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
+
+    @Override
+    public void sendComment(Comment comment) {
+        System.out.println("Sending push notification for comment : " + comment.getText());
+    }
+}
---
+++ Spring-05-Core-BeanScope/src/main/java/com/ceva/service/CommentService.java
@@ -0,0 +1,24 @@
+package com.ceva.service;
+
+import com.ceva.model.Comment;
+import com.ceva.proxy.CommentNotificationProxy;
+import com.ceva.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+    }
+
+}
