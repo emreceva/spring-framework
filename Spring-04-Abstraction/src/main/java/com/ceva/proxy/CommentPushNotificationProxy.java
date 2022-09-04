60198157fa9beda13429a273e43c31aab889c5be
emreceva <86777959+emreceva@users.noreply.github.com>
2022-09-04 12:07:33 +0300

    Qualifier

--- Spring-04-Abstraction/src/main/java/com/ceva/proxy/CommentPushNotificationProxy.java
+++ Spring-04-Abstraction/src/main/java/com/ceva/proxy/CommentPushNotificationProxy.java
@@ -2,16 +2,14 @@ package com.ceva.proxy;
 
 import com.ceva.model.Comment;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Primary
-@Qualifier("EMAIl")
-public class EmailCommentNofiticationProxy implements CommentNotificationProxy{
+@Qualifier("PUSH")
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
 
     @Override
     public void sendComment(Comment comment) {
-        System.out.println("Sending notification for comment" + comment.getText());
+        System.out.println("Sending push notification for comment : " + comment.getText());
     }
 }
