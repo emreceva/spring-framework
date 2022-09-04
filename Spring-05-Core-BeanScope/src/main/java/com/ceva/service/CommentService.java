b28d9e726c11784983c0246dce40648d8c505941
emreceva <86777959+emreceva@users.noreply.github.com>
2022-09-04 12:48:20 +0300

    Bean Scope

--- Spring-05-Core-BeanScope/src/main/java/com/ceva/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/com/ceva/service/CommentService.java
@@ -3,10 +3,17 @@ package com.ceva.service;
 import com.ceva.model.Comment;
 import com.ceva.proxy.CommentNotificationProxy;
 import com.ceva.repository.CommentRepository;
+import org.springframework.beans.factory.BeanDefinitionStoreException;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Lazy
 public class CommentService {
 
     private final CommentRepository commentRepository;
@@ -15,6 +22,7 @@ public class CommentService {
     public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
+        System.out.println("Hello");
     }
     public void publishComment(Comment comment){
         commentRepository.storeComment(comment);
