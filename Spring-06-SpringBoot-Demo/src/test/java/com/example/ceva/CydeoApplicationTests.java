269eb243cb37dca80dc72302da330b3835b29a98
emreceva <86777959+emreceva@users.noreply.github.com>
2022-09-06 17:36:03 +0300

    Spring Boot Demo

---
+++ Spring-06-SpringBoot-Demo/src/test/java/com/example/ceva/CydeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ceva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CydeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
