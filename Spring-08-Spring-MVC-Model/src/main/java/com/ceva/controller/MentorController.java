fdbf8f61740988e07ea4cf2fb1db1ce1c80a5b00
emreceva <86777959+emreceva@users.noreply.github.com>
2022-09-09 00:31:38 +0300

    Spring MVC

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/ceva/controller/MentorController.java
@@ -0,0 +1,29 @@
+package com.ceva.controller;
+
+import com.ceva.enums.Gender;
+import com.ceva.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @RequestMapping("/list")
+    public String showTable(Model model){
+
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Mike", "Smith", 45, Gender.MALE));
+        mentorList.add(new Mentor("Tom", "Hanks", 50, Gender.MALE));
+        mentorList.add(new Mentor("Amy", "Bryan", 30, Gender.FEMALE));
+
+        model.addAttribute("mentors", mentorList);
+
+        return "mentor/mentor-list";
+
+    }
+}
--- Spring-08-Spring-MVC-Model/src/main/java/com/ceva/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/ceva/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.ceva.controller;
 
+import com.ceva.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -10,11 +15,25 @@ public class StudentController {
     @RequestMapping("/welcome")
     public String homePage(Model model){
 
-        //Model methods
-        model.addAttribute("name", "Ceva");
-        model.addAttribute("course" , "MVC");
-        model.addAttribute("subject", "");
+        model.addAttribute("name","Cydeo");
+        model.addAttribute("course","MVC");
 
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+
+        //create some random studentId(0-1000) and show it in your UI
+        int studentId=new Random().nextInt();
+        model.addAttribute("id",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        numbers.add(10);
+        model.addAttribute("numbers",numbers);
+
+        Student student = new Student(1,"Mike","Smith");
+        model.addAttribute("student",student);
 
         return "student/welcome";
     }
